6b48fa7faabe244b2fd93a54dc0febeaca69aaa4
jaceara2006 <jaceara2006@g.educaand.es>
2023-12-14 11:11:52 +0100

    Metodo Uno y Dos Creados

--- src/main/java/daw/Ej1.java
+++ src/main/java/daw/Ej1.java
@@ -1,14 +1,37 @@
 package daw;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author acebedo
  */
 public class Ej1 {
+
     public static void main(String[] args) {
-        
-        
-        
+
     }
-    
+
+    public static int numeroPalabrasInicial() {
+        int numeroPalabras = 0;
+        boolean repetir = false;
+
+        do {
+            try {
+                numeroPalabras = Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de palabras que vas a meter."));
+                repetir = false;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Introduce un valor correcto.");
+            }
+        } while (repetir);
+
+        return numeroPalabrasInicial();
+    }
+
+    public static String crearArrayNumeroPalbras() {
+        
+        // Crear un array con el tamaño adecuado.
+        String[] arrayPalabras = new [];
+    }
+
 }
