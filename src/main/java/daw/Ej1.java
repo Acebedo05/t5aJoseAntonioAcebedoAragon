5766c41741984a5570e8ceaa2f87790000444285
jaceara2006 <jaceara2006@g.educaand.es>
2023-12-14 12:52:53 +0100

    Metodos Uno Dos y Tres completados

--- src/main/java/daw/Ej1.java
+++ src/main/java/daw/Ej1.java
@@ -10,28 +10,45 @@ public class Ej1 {
 
     public static void main(String[] args) {
 
+        String arrayNumeroPalabras[] = crearArrayNumeroPalabras();
+        mostrarArrayNumeroPalabras(arrayNumeroPalabras);
+
     }
 
     public static int numeroPalabrasInicial() {
         int numeroPalabras = 0;
-        boolean repetir = false;
+        boolean repetir = true;
 
         do {
             try {
                 numeroPalabras = Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de palabras que vas a meter."));
                 repetir = false;
             } catch (NumberFormatException nfe) {
-                System.out.println("Introduce un valor correcto.");
+                JOptionPane.showMessageDialog(null, "Introduce un valor correcto.");
             }
         } while (repetir);
 
-        return numeroPalabrasInicial();
+        return numeroPalabras;
     }
 
-    public static String crearArrayNumeroPalbras() {
-        
+    public static String[] crearArrayNumeroPalabras() {
+
         // Crear un array con el tamaño adecuado.
-        String[] arrayPalabras = new [];
+        String[] arrayPalabras = new String[numeroPalabrasInicial()];
+
+        for (int i = 0; i < arrayPalabras.length; i++) {
+            arrayPalabras[i] = JOptionPane.showInputDialog(null, "Introduce una palabra.");
+        }
+        return arrayPalabras;
     }
 
+    public static void mostrarArrayNumeroPalabras(String[] arrayPalabras) {
+        String aux = "";
+
+        for (int i = 0; i < arrayPalabras.length; i++) {
+            aux += "Posicion" + i + " - Palabra: " + arrayPalabras[i] + "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, aux);
+    }
 }
